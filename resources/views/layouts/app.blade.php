0244cc621cd2a7edda0b37b1d4e25c72f12baef4
jimmy1724524143 <jeims-sk@live.com>
2024-04-09 16:14:44 -0500

    Loginv1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,10 +16,11 @@
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
-<body>
+<body >
+
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
+            <div class="container"  >
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
