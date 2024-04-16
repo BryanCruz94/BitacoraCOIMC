6695c0f201809188537becaa108976d925518930
dakrosP2 <djdakrosmix2307@gmail.com>
2024-04-16 09:33:27 -0500

    admin3

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -13,31 +13,31 @@
 @stop
 
 @section('content')
-<div class="main-content" style="background-image: url('{{ asset('vendor/adminlte/dist/img/intro.jpg') }}'); background-size: cover; padding: 52px;">        
+<div class="main-content" style="background-image: url('{{ asset('vendor/adminlte/dist/img/home.jpg') }}'); background-size: cover; padding: 60px;">        
     <div class="row justify-content-center">
             <div class="col-lg-6 col-sm-12 col-md-7 mb-5"> 
                 <a href="{{route('vehicles.index')}}" class="btn btn-lg d-flex flex-column align-items-center">
-                <img src="{{ asset('vendor/adminlte/dist/img/vehiculo.png') }}" alt="Conductor" style="width: 100px; height: 100px; object-fit: cover; border-radius: 30%;">                
+                <img src="{{ asset('vendor/adminlte/dist/img/vehiculo.png') }}" alt="Conductor" style="width: 100px; height: 100px; object-fit: cover; border-radius: 20%;">                
                     <span style="font-size: 15px; color: black; font-weight: bold;">VEHICULAR </span>
                 </a>
             </div>
 
             <div class="col-lg-6 col-sm-12 col-md-7 mb-5">
                 <a href="#" class="btn btn-lg d-flex flex-column align-items-center">
-                <img src="{{ asset('vendor/adminlte/dist/img/conductor.png') }}" alt="Conductor" style="width: 100px; height: 100px; object-fit: cover; border-radius: 30%;">
+                <img src="{{ asset('vendor/adminlte/dist/img/conductor.png') }}" alt="Conductor" style="width: 100px; height: 100px; object-fit: cover; border-radius: 20%;">
                     <span style="font-size: 15px; color: black; font-weight: bold;">CONDUCTORES</span>
                 </a>
             </div>
 
             <div class="col-lg-6 col-sm-12 col-md-7 mb-5">
                 <a href="#" class="btn btn-lg d-flex flex-column align-items-center">
-                <img src="{{ asset('vendor/adminlte/dist/img/usuario.png') }}" alt="Conductor" style="width: 100px; height: 100px; object-fit: cover; border-radius: 30%;">
+                <img src="{{ asset('vendor/adminlte/dist/img/usuario.png') }}" alt="Conductor" style="width: 100px; height: 100px; object-fit: cover; border-radius: 20%;">
                     <span style="font-size: 15px; color: black; font-weight: bold;">USUARIOS</span>
                 </a>
             </div>
             <div class="col-lg-6 col-sm-12 col-md-7 mb-5">
                 <a href="#" class="btn btn-lg d-flex flex-column align-items-center">
-                <img src="{{ asset('vendor/adminlte/dist/img/cuartel.png') }}" alt="Conductor" style="width: 100px; height: 100px; object-fit: cover; border-radius: 30%;">
+                <img src="{{ asset('vendor/adminlte/dist/img/cuartel.png') }}" alt="Conductor" style="width: 100px; height: 100px; object-fit: cover; border-radius: 20%;">
                     <span style="font-size: 15px; color: black; font-weight: bold;">UNIDAD MILITAR</span>
                 </a>
             </div>
